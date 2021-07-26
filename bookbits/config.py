5c707acf1c8433ea98376bcb2e9e8653b417bfcb
Meris Čulić <meris.culic@gmail.com>
2021-07-26 14:45:13 +0200

    Fixing heroku database issue

--- bookbits/config.py
+++ bookbits/config.py
@@ -2,11 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DB_URI', "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'  # TODO: Change mail server value if ML acc not on gmail
+    MAIL_SERVER = 'smtp.googlemail.com'  # TODO: Change mail server value if BB acc not on gmail
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')  # TODO: Create ML e-mail acc and change env vars in os
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')  # TODO: Create BB e-mail acc and change env vars in os
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
--- bookbits/models.py
+++ bookbits/models.py
@@ -39,6 +39,7 @@ class User(db.Model, UserMixin):
 
 
 class Messages(db.Model):
+    __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
     sender_email = db.Column(db.String)
     sender_name = db.Column(db.String)
